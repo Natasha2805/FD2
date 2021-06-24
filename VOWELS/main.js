29cc844b7000258f3b0ae9107faaf242eaaec40b
natasha <nataliasolox@gmail.com>
2021-06-24 14:52:33 +0300

    fix: task VOWELS

--- VOWELS/main.js
+++ VOWELS/main.js
@@ -4,7 +4,7 @@
 /*function findWord(){
     let stringUser = prompt('введите сроку');
     let count = 0;
-    let arrString = ["а", "о","и", "е", "ё", "э", "ы", "у", "ю", "я"];
+    let arrString = ["а", "о","и", "е", "ё", "э", "ы", "у", "ю", "я","А","О","И","Е","Ё","Ы","У","Ю","Я"];
     for (let i = 0; i< stringUser.length; i++ ){
         for (let j=0; j<arrString.length;j++){
            if(stringUser[i] === arrString[j]){
