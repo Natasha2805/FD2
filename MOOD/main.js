0093771be3d0c2908ad82a9f2f651594832a4061
natasha <nataliasolox@gmail.com>
2021-07-30 21:19:42 +0300

    feat: task MOOD

--- MOOD/main.js
+++ MOOD/main.js
@@ -6,22 +6,17 @@ function randomDiap(n,m) {
 }
 
 function mood(colorsCount) {
-var colors={
-    1: 'красный',
-    2: 'оранжевый',
-    3: 'жёлтый',
-    4: 'зелёный',
-    5: 'голубой',
-    6: 'синий',
-    7: 'фиолетовый'
-}
-let newobj={};
+    var colors = [,'красный','оранжевый','жёлтый','зелёный','голубой','синий','фиолетовый']
+    let newobj={};
 console.log( 'цветов: ' + colorsCount );
 for ( var i=1; i<=colorsCount; i++ ) {
-    var n=randomDiap(1,7);
+    do {
+        var n=randomDiap(1,7); 
+    }
+    while(n in newobj)
+    newobj[n] = true;
     var colorName=colors[n];
     console.log( colorName );
 }
 }
-
 mood(3);
